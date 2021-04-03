f84440b4ca2b750710d92972ec97dfb6c7515817
Crowdbotics <team@crowdbotics.com>
2021-04-03 19:37:55 +0000

    Initial commit.

---
+++ ios/hybridevaluationfor_25447Tests/hybridevaluationfor_25447Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface hybridevaluationfor_25447 : XCTestCase
+
+@end
+
+@implementation hybridevaluationfor_25447
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://hybridevaluationfor-25447.botics.co",
+  defaultTimeout: 5000
+};
